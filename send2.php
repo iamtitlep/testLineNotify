31c150a2e26077b5c4019155161c4db90fce2b39
topazds <top.p_1507@windowslive.com>
2018-01-26 14:03:53 +0700

    Update send2.php

--- send2.php
+++ send2.php
@@ -33,36 +33,57 @@ $token = 'VG10VkKCRJ2Gy3BFEb6TNJKAvNa8arkXvFLeWjSIzyy';
 //   return $result;
 // }
 
+<?php
+//Get POST body content 
+// $content = file_get_contents('php://input');
+// //Parse JSON 
+// $events = json_decode($content,true);
+//$title = $events['payload']['title']
+// $message = $events['payload']['title']
+$message = "เทสนะ";
+$token = '7ZRdYLpa1fUzF1Mk9m4s8jLA9nWiM98VRMcxGmsBveR';
+ // echo send_line_notify($message, $token);
+ // function send_line_notify($message, $token)
+ // {
+ //   $ch = curl_init();
+ //   curl_setopt( $ch, CURLOPT_URL, "https://notify-api.line.me/api/notify");
+ //   curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0);
+ //   curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0);
+ //   curl_setopt( $ch, CURLOPT_POST, 1);
+ //   curl_setopt( $ch, CURLOPT_POSTFIELDS, "message=$message");
+ //   curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+ //   $headers = array( "Content-type: application/x-www-form-urlencoded", "Authorization: Bearer $token", );
+ //   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+ //   curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+ //   $result = curl_exec( $ch );
+ //   curl_close( $ch );
+ //   return $result;
+ // }
+
 $url = 'https://notify-api.line.me/api/notify';
 $data = ['message' => $message];
 $headers = [
     'Accept: */*',
     'Content-Type: application/x-www-form-urlencoded',
-    'Authorization: Bearer $token'
+    'Authorization: Bearer 7ZRdYLpa1fUzF1Mk9m4s8jLA9nWiM98VRMcxGmsBveR'
 ];
-
 // open connection
 $ch = curl_init();
-
 // set curl options
 $options = [
     CURLOPT_URL => $url,
     CURLOPT_POST => count($data),
-    CURLOPT_SSL_VERIFYHOST => 0,
-    CURLOPT_SSL_VERIFYPEER => 0,
     CURLOPT_POSTFIELDS => http_build_query($data),
     CURLOPT_HTTPHEADER => $headers,
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_FOLLOWLOCATION => 1,
 ];
 curl_setopt_array($ch, $options);
-
 // execute
 $result = curl_exec($ch);
-
 // close connection
 curl_close($ch);
-
 echo $result;
+?>
 
 ?>
